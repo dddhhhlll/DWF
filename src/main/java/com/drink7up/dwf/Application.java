6422e96e4a5e72f026729ed9a185e1935a4de3c2
dddhhhlll <466273263@qq.com>
2017-04-18 17:43:05 +0800

    test
    Signed-off-by: dddhhhlll <466273263@qq.com>

---
+++ src/main/java/com/drink7up/dwf/Application.java
@@ -0,0 +1,17 @@
+package com.drink7up.dwf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created  by hlding on 2017/4/18.
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
+    }
+}
